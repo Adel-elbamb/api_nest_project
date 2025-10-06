6d948c7b2c85d81312d08fa8084199e57d577d4e
Adel-elbamb <engadel987654321@gmail.com>
2025-10-06 12:51:16 +0300

    feat(chat) create the Authintcation chat and send private message

--- api_project/src/modules/chat/chat.gateway.ts
+++ api_project/src/modules/chat/chat.gateway.ts
@@ -1,28 +1,58 @@
-import { OnModuleInit, } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer, MessageBody } from '@nestjs/websockets';
-import { Server } from 'socket.io';
-@WebSocketGateway()
-export class ChatGateway implements OnModuleInit {
+// src/chat/chat.gateway.ts
+import {
+  WebSocketGateway,
+  OnGatewayConnection,
+  ConnectedSocket,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer
+} from "@nestjs/websockets";
+import { JwtService } from "@nestjs/jwt";
+import { Socket, Server } from "socket.io";
+
+@WebSocketGateway({ cors: true })
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
-  onModuleInit() {
-    // socket have all data for user connection 
-    this.server.on('connection', (socket) => {
-      console.log(`Client connected: ${socket.id}`);
-    })
+  private onlineUsers = new Map(); // username -> socket.id
 
+  constructor(private jwtService: JwtService) { }
+
+  handleConnection(client: Socket) {
+    try {
+      // Read the token from header instead of auth
+      const token = client.handshake.headers['authorization']?.split(' ')[1];
+      if (!token) throw new Error('No token provided');
+
+      const user = this.jwtService.verify(token);
+      client.data.user = user;
+      // console.log(user)
+      this.onlineUsers.set(user.name, client.id);
+      console.log(this.onlineUsers)
+      // console.log(client.id)
+      console.log(` ${user.name} connected`);
+    } catch (err) {
+      console.log("invalid token:", err.message);
+      client.disconnect();
+    }
   }
 
-  @SubscribeMessage('sendMore')
-  sendMore(@MessageBody() massage: string) {
-    this.server.emit('newSend', massage);
-
-
+  @SubscribeMessage("privateMessage")
+  handlePrivateMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: { to: string; message: string }
+  ) {
+    const sender = client.data.user.username;
+    const receiverSocketId = this.onlineUsers.get(data.to);
+    console.log(receiverSocketId)
+    if (receiverSocketId) {
+      client.to(receiverSocketId).emit("privateMessage", {
+        from: sender,
+        message: data.message,
+      });
+    }
   }
 
-  @SubscribeMessage('sendOne')
-  sendOne(@MessageBody() data: { userId: string, message: string }) {
-    this.server.to(data.userId).emit('sendSingleUser', data.message);
-  }
+
 
 }
